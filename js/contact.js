dcab8e8980e52d764a788ddd40c8e21e1a9aacb2
Kavin Subramanyam <kavin.subramanyam@students.makeschool.com>
2016-01-19 14:42:44 -0800

    Add formspree implementation

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,20 @@
 $(document).ready( function(){
-	
+	$('form').submit(function(e){
+		e.preventDefault();
+		$.ajax({
+		    url: "//formspree.io/kavin.a.subramanyam@gmail.com", 
+		    method: "POST",
+		    data: { 
+		    	email: $('email').val(),
+		    	message: $('message').val()
+		    },
+		    dataType: "json"
+		}).error(function(jqXHR, status, error){
+			alert('Error while submitting.');
+		}).success(function(data, status, jqXHR){
+			alert('Successfully submitted.');
+			$('form').find('.email, .message').val('');
+		});
+	});
 });
+
